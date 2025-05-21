fd01d016e44d2b168e1c8010624e5d831063d3f2
Hesham Alhuraibi <heshamalhuraibi1@gmail.com>
2025-05-22 01:08:57 +0200

    Ensure app works with tailwind and daisy

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="card w-96 bg-base-100 card-xs shadow-sm">
+      <div class="card-body">
+        <h2 class="card-title">Xsmall Card</h2>
+        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+        <div class="justify-end card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
